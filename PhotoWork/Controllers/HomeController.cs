4548ec257a40520708b1ad56796fb0ca8a9e8190
tungbb159 <thanhgodlike@gmail.com>
2021-03-10 21:59:08 +0700

    Tung register

--- PhotoWork/Controllers/HomeController.cs
+++ PhotoWork/Controllers/HomeController.cs
@@ -1,8 +1,10 @@
-﻿using PhotoWork.Models;
+﻿using PhotoWork.DTO;
+using PhotoWork.Models;
 using System;
 using System.Collections.Generic;
 using System.Data.SqlClient;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Web;
 using System.Web.Configuration;
 using System.Web.Mvc;
@@ -12,6 +14,7 @@ namespace PhotoWork.Controllers
 {
     public class HomeController : Controller
     {
+        string con = @"server=SE140240\SQLEXPRESS;database=PhotoWork;uid=sa;pwd=123456";
         /* [Authorize(Roles = "Admin")]*/
         public ActionResult Index()
         {
@@ -19,7 +22,66 @@ namespace PhotoWork.Controllers
         }
 
 
-      
+        public ActionResult Register(string returnUrl)
+        {
+            bool check = true;
+            Session.Remove("ERROR");
+            string email = Request.Form["email"];
+            string password = Request.Form["password"];
+            string rePassword = Request.Form["rePassword"];
+            string role = Request.Form["role"];
+            string phone = Request.Form["phone"];
+            string fullName = Request.Form["fullName"];
+            RegisterError error = new RegisterError();
+            if (!Regex.IsMatch(email, "[a-zA-Z]{1}[a-zA-Z0-9]{0,15}@gmail\\.com"))
+            {
+                check = false;
+                error.email= "Email phải thuộc dạng [tên]@gmail.com";
+            }
+
+            if (!password.Equals(rePassword))
+            {
+                check = false;
+                error.password = "Xác nhận password sai!";
+            }
+
+            if (!Regex.IsMatch(phone, "0[0-9]{9}"))
+            {
+                check = false;
+                error.phone = "Số điện thoại bắt đầu từ 0 và có 10 số";
+            }
+
+
+            if (check)
+            {
+                SqlConnection connection = new SqlConnection(con);
+            string SQL = "insert into AuthenticatedUser(email, passwords, role, phoneNumber, fullName,isActive,isBanned,Avatar) values(@email,@pass,@role,@phone,@name,1,0,'1')";
+            SqlCommand command = new SqlCommand(SQL,connection);
+            command.Parameters.AddWithValue("@email", email);
+            command.Parameters.AddWithValue("@pass", password);
+            command.Parameters.AddWithValue("@role", role);
+            command.Parameters.AddWithValue("@phone", phone);
+            command.Parameters.AddWithValue("@name", fullName);
+            connection.Open();
+            int n=command.ExecuteNonQuery();
+            connection.Close();
+             
+            SQL = "insert into Photographer(username) values(@email)";
+            command = new SqlCommand(SQL, connection);
+            command.Parameters.AddWithValue("@email", email);
+            connection.Open();
+            command.ExecuteNonQuery();
+            connection.Close();
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                Session.Add("ERROR", "have error");
+                
+                return RedirectToAction("Index", "Home", new { id = 1, emailError = error.email, confirmError = error.password, phoneError = error.phone });
+            }
+                
+        }
         [HttpPost]
         public ActionResult Login(string returnUrl)
         {
@@ -34,11 +96,12 @@ namespace PhotoWork.Controllers
              */
             String Email = Request.Form["email"];
             String Password = Request.Form["password"];
+            Session.Remove("Account_ERR");
             PhotoWorkEntities db = new PhotoWorkEntities();
             var dataItem = db.AuthenticatedUsers.Where(x => x.Email == Email && x.passwords == Password).FirstOrDefault();
             if (dataItem != null)
             {
-                Session["Account_ERR"] = "";
+                
                 FormsAuthentication.SetAuthCookie(dataItem.Email, false);  //set Cookie 
                 Session["ROLE"] = dataItem.Role;  //set session 
 
